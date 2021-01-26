7821b16897da64fdcf56d19e5287e90c3b56d79a
Scott Doggett <sdoggett@lcs.on.ca>
2021-01-25 19:37:42 -0500

    Added tips and map view, add tab bar to switch between them.

---
+++ Shared/Views/TipsList.swift
@@ -0,0 +1,20 @@
+//
+//  TipsList.swift
+//  TrekrMultiPlatform
+//
+//  Created by Doggett, Scott on 2021-01-25.
+//
+
+import SwiftUI
+
+struct TipsList: View {
+    var body: some View {
+        Text("Hello TipsList view!")
+    }
+}
+
+struct TipsList_Previews: PreviewProvider {
+    static var previews: some View {
+        TipsList()
+    }
+}
---
+++ Shared/Views/WorldMap.swift
@@ -0,0 +1,20 @@
+//
+//  WorldMap.swift
+//  TrekrMultiPlatform
+//
+//  Created by Doggett, Scott on 2021-01-25.
+//
+
+import SwiftUI
+
+struct WorldMap: View {
+    var body: some View {
+        Text("Hello, WorldMap view!")
+    }
+}
+
+struct WorldMap_Previews: PreviewProvider {
+    static var previews: some View {
+        WorldMap()
+    }
+}
